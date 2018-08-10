eb8455a471dd0ed402dc156d2bd40775bf804687
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-10 13:02:32 +0530

    Create searches.java

---
+++ searches.java
@@ -0,0 +1,19 @@
+//1.linear search 
+public class Linear{
+public static void linear(int[] arr,int key){
+int n = arr.length;int flag = 0;
+for(int i = 0 ; i < n;i++){
+  if(arr[i]==key) {System.out.println("yeah");break;}
+       
+      }
+      if(flag==n)System.out.println("nah");
+     }
+      
+      
+
+public static void main(String[] args){
+	int arr[] = { 5,2,8,3,9};
+	 int key = 2;
+	 linear(arr,key);
+                }
+                }
