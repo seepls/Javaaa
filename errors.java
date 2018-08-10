2be76f6eb774636eff6866c0d674dfc87f1cdfa9
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-10 10:47:02 +0530

    Update errors.java

--- errors.java
+++ errors.java
@@ -1,5 +1,6 @@
 // posible set of errors 
 
+//1.
 class Simple{  
 public static void main(String[] args){  
 byte a=10;  
@@ -9,7 +10,7 @@ byte c=(byte)(a+b);
 System.out.println(c);  
 }}  
 
-
+//2.
 class OperatorExample{  
 public static void main(String args[]){  
 short a=10;  
@@ -18,3 +19,16 @@ short b=10;
 a=a+b;//Compile time error because 10+10=20 now int  
 System.out.println(a);  
 }}  
+
+//3. if u initialize m = 0 ; fuck all ! -_-
+public class Fact{
+public static void main(String[] args){
+int n= 5,m=1 ;// number to be checked .
+
+for(int i =1 ; i <= n ; i++ ) {
+
+ m *=i;
+ }
+ System.out.println(n+"factorial is " + m);
+ }
+ }
