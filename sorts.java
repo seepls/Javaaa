cf9ac8bbdbfe546aa13e2347407b3befe57966c0
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-10 12:20:13 +0530

    Update sorts.java

--- sorts.java
+++ sorts.java
@@ -29,3 +29,41 @@ System.out.println("Array After Bubble Sort");
                 
 }
 }
+
+//2.selection sort 
+// locate the min from the rest and place the element in right position .swap the current element with the next lower element 
+import java.io.*;
+import java.lang.*;
+public class Selection{
+public static void selection (int[] arr){
+int j = 0 ;
+for(int i = 0; i<arr.length-1 ; i++)
+{
+int index =i; 
+int min=arr[i];
+for(j =i+1;j<arr.length ;j++){
+ 
+if(arr[j] <arr[index] ){
+ index=j;
+ }}
+ min=arr[index];
+ arr[index]=arr[i];
+ arr[i]=min;
+ }
+}
+	      
+public static void main(String[] args){
+int arr[] = {3,2,7,4,5,8};
+ System.out.println("Array Before selection Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                
+selection(arr);
+System.out.println("Array After selection Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                
+}
+}
