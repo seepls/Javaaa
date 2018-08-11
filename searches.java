62ed348d1d8941a2617dfe48c8fcfc7fa3100cd9
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-11 21:12:59 +0530

    Update searches.java

--- searches.java
+++ searches.java
@@ -17,3 +17,23 @@ public static void main(String[] args){
 	 linear(arr,key);
                 }
                 }
+//2.binary search : searching a sorted array by repeatedly divide the array into halfs 
+import java.io.*;
+class Binary{
+public static int binary (int arr[] , int l , int r , int x){
+ if(r>=l){
+ int  mid = l + (r-l)/2;
+ if (arr[mid]==x)return x;
+ if(arr[mid]>x)return binary(arr , l , mid-1 ,x);
+ if(arr[mid]<x)return binary(arr,mid+1,r,x);
+ }return -1;
+ }
+ public static void main(String[] args){
+ Binary ob = new Binary() ; 
+ int arr[] = {29,9,23 ,1999,12,13,14 } ;
+ int n = arr.length;
+ int x =1999 ;
+ int result = binary (arr , 0 , n-1 , x);
+ if(result== -1 ) {System.out.println("not present ");}
+ else System.out.println("element found " + result);
+ }}
