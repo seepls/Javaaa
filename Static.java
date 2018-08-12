66b2f96e3e946d33b0e3be70910ce467b5e63e4f
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-13 00:47:55 +0530

    Create Static.java

---
+++ Static.java
@@ -0,0 +1,48 @@
+// used for memory management mainly 
+// belongs to the class than the instance of a class 
+// can be method(class method) , variable(class variable) , nested method ,block.
+// static variable :used to refrence those properties of all object (not unique for each object )
+// gets memory only once in class area , during class loading
+//advantage of static variable is that it makes the program memory efficient 
+//Java static property is shared to all objects.
+
+
+// counter without static variable 
+class Counter{  
+int count=0;//will get memory when instance is created  
+  
+Counter(){  
+count++;  
+System.out.println(count);  
+}  
+  
+public static void main(String args[]){  
+  
+Counter c1=new Counter();  
+Counter c2=new Counter();  
+Counter c3=new Counter();  
+  
+ }  
+}  
+
+//output : 1 1 1 
+
+//counter with static variable 
+class Counter2{  
+static int count=0;//will get memory only once and retain its value  
+  
+Counter2(){  
+count++;  
+System.out.println(count);  
+}  
+  
+public static void main(String args[]){  
+  
+Counter2 c1=new Counter2();  
+Counter2 c2=new Counter2();  
+Counter2 c3=new Counter2();  
+  
+ }  
+}  
+
+//output : 1 2 3 
