767b2b88b7b0a5bd35c49b89a0ce3fe7bc24ac5d
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-13 00:22:26 +0530

    Create create-object.java

---
+++ create-object.java
@@ -0,0 +1,13 @@
+// anonymous object
+class Calculation{  
+ void fact(int  n){  
+  int fact=1;  
+  for(int i=1;i<=n;i++){  
+   fact=fact*i;  
+  }  
+ System.out.println("factorial is "+fact);  
+}  
+public static void main(String args[]){  
+ new Calculation().fact(5);//calling method with anonymous object  
+}  
+}  
