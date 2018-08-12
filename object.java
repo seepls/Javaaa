96e9b2851588ad4b9a176f4782acec43f0119cb5
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-13 00:20:45 +0530

    Update object.java

--- object.java
+++ object.java
@@ -22,3 +22,37 @@ class TestStudent1{
  }  
 }  
 
+// 3 ways to initialize a object 
+//1. initialize thorgh refrence 
+class Student{  
+ int id;  
+ String name;  
+}  
+class TestStudent2{  
+ public static void main(String args[]){  
+  Student s1=new Student();  
+  s1.id=101;  
+  s1.name="Sonoo";  
+  System.out.println(s1.id+" "+s1.name);//printing members with a white space  
+ }  
+}  
+//2.through a method
+class Student{  
+ int rollno;  
+ String name;  
+ void insertRecord(int r, String n){  
+  rollno=r;  
+  name=n;  
+ }  
+ void displayInformation(){System.out.println(rollno+" "+name);}  
+}  
+class TestStudent4{  
+ public static void main(String args[]){  
+  Student s1=new Student();  
+  Student s2=new Student();  
+  s1.insertRecord(111,"Karan");  
+  s2.insertRecord(222,"Aryan");  
+  s1.displayInformation();  
+  s2.displayInformation();  
+ }  
+}  
