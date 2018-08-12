bb800b195ce0c8c34f59192871d0ca9e749a43f1
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-13 00:18:34 +0530

    Update object.java

--- object.java
+++ object.java
@@ -9,3 +9,16 @@ class Student{
   System.out.println(s1.name);  
  }  
 }  
+//2. main funciton outside class 
+class Student{  
+ int id;  
+ String name;  
+}  
+class TestStudent1{  
+ public static void main(String args[]){  
+  Student s1=new Student();  
+  System.out.println(s1.id);  
+  System.out.println(s1.name);  
+ }  
+}  
+
