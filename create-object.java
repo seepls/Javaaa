285799c9b10cb51a2990184aaa2ed5d73381dd96
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-13 00:25:02 +0530

    Update create-object.java

--- create-object.java
+++ create-object.java
@@ -11,3 +11,7 @@ public static void main(String args[]){
  new Calculation().fact(5);//calling method with anonymous object  
 }  
 }  
+
+//creating multiple objects by one type only 
+// initialization of primitive e.i : int a=10, b=20;  
+//initialization of refrence variables Rectangle r1=new Rectangle(), r2=new Rectangle();//creating two objects  
