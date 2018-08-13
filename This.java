1f6655f59985e7ec3ffd65d5d641a559e8d4cb86
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-13 07:10:41 +0530

    Update This.java

--- This.java
+++ This.java
@@ -69,3 +69,28 @@ class TestThis6{
 public static void main(String args[]){  
 A a=new A();  
 }}  
+
+
+//usage of this in constructor call
+class Stundent
+{
+  int roll;
+  String name ,course ;
+  float fee ;
+  Student (int roll , String name , String course ){
+    this.roll = roll ; 
+    this.name = name ;
+    this.course = course ;
+  }
+  Student (int roll ,String name ,String course , float fee ){
+    this.fee = fee ;
+    Student(roll , name , course );
+  }
+  void display(){System.out.println(roll + " " + name + "  "+ course + " " + fee);}
+  class Test{
+    public static void main(String args){
+      Student s1=new Student(111,"smriti","java");  
+Student s2=new Student(112,"prashanthi","java",6000f);  
+s1.display();  
+s2.display();  
+}}  
