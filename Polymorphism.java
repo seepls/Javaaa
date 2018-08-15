9cf0679ccd3d5ea9e040a79ca44da22723808d28
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-16 03:51:23 +0530

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -44,3 +44,65 @@ System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest());
 // can we override static methods ? NO , can be explained by runtime polymorphism 
 //why ? cause static method is bound class , where as instance methid is bound to object . static belogs to class area where as instance belongs to heap area .
 // so , we cant over ride main method as is public static . 
+
+
+
+// covarient return type : specifies that the return type may vary in the same direction as the subclass .
+//it is possible to override method by changing the return type if subclass overrides any method whose ,
+//return type is Non-Primitive but it changes its return type to subclass type.
+
+//super refers to the immidiate parent class of the this class .
+//1. super used to refer immidiate parent class instance variable 
+
+class Animal {
+  String color = " white " ;
+}
+class Dog extends Animal {
+  String color = "black" ;
+  void printColor(){
+    System.out.println(color);
+    System.out.println(super.color);
+  }
+}
+class TestSuper{
+  public static void main(String args[]){
+    Dog d = new Dog();
+    d.printColor();
+  }
+}
+
+
+// 2. super class can be used to invoke parent class method 
+class Animal{  
+void eat(){System.out.println("eating...");}  
+}  
+class Dog extends Animal{  
+void eat(){System.out.println("eating bread...");}  
+void bark(){System.out.println("barking...");}  
+void work(){  
+super.eat();  
+bark();  
+}  
+}  
+class TestSuper2{  
+public static void main(String args[]){  
+Dog d=new Dog();  
+d.work();  
+}}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
