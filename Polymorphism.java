24c94330ce1f1e189613a8d559eb6e400cb2e952
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-16 03:37:32 +0530

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -12,3 +12,35 @@ class OverloadingCalculation3{
 }  
 
 //One type is not de-promoted implicitly for example double cannot be depromoted to any type implicitly.
+____________________
+// method overriding : when same method as DECLARED  in parent class ,  defined differently in subclass 
+class Bank{  
+int getRateOfInterest(){return 0;}  
+}  
+  
+class SBI extends Bank{  
+int getRateOfInterest(){return 8;}  
+}  
+  
+class ICICI extends Bank{  
+int getRateOfInterest(){return 7;}  
+}  
+class AXIS extends Bank{  
+int getRateOfInterest(){return 9;}  
+}  
+  
+class Test2{  
+public static void main(String args[]){  
+SBI s=new SBI();  
+ICICI i=new ICICI();  
+AXIS a=new AXIS();  
+System.out.println("SBI Rate of Interest: "+s.getRateOfInterest());  
+System.out.println("ICICI Rate of Interest: "+i.getRateOfInterest());  
+System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest());  
+}  
+}  
+
+
+// can we override static methods ? NO , can be explained by runtime polymorphism 
+//why ? cause static method is bound class , where as instance methid is bound to object . static belogs to class area where as instance belongs to heap area .
+// so , we cant over ride main method as is public static . 
