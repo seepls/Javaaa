8d2a28ccf08726a980ab3d5bba7c74e9eec8de2c
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-16 03:29:49 +0530

    Create Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,12 @@
+// method overloading :mthods having same name but different parameters 
+// type promotion : One type is promoted to another implicitly if no matching datatype is found. 
+// in case of ambiguity 
+class OverloadingCalculation3{  
+  void sum(int a,long b){System.out.println("a method invoked");}  
+  void sum(long a,int b){System.out.println("b method invoked");}  
+  
+  public static void main(String args[]){  
+  OverloadingCalculation3 obj=new OverloadingCalculation3();  
+  obj.sum(20,20);//now ambiguity  
+  }  
+}  
