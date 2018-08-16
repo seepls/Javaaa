a1cea5a64f7f385cb31b031b7e16c02b5dea9936
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-16 12:00:58 +0530

    Update Final.java

--- Final.java
+++ Final.java
@@ -42,3 +42,27 @@ class Honda1 extends Bike{
 
 
 //final method is inherited but you cannot override it.
+
+
+
+
+
+//A final variable that is not initialized at the time of declaration is known as blank final variable.
+//If you want to create a variable that is initialized at the time of creating object and once initialized may not be changed, it is useful
+// we can initialize finalblanck variable only in constructor 
+class Bike10{  
+  final int speedlimit;//blank final variable  
+    
+  Bike10(){  
+  speedlimit=70;  
+  System.out.println(speedlimit);  
+  }  
+  
+  public static void main(String args[]){  
+    new Bike10();  
+ }  
+}  
+
+
+//A static final variable that is not initialized at the time of declaration is known as static blank final variable.
+//It can be initialized only in static block.
