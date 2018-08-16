ef8be8ad1a13451a3745a0012386b21334592e41
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-16 18:13:33 +0530

    Create Package.java

---
+++ Package.java
@@ -0,0 +1,7 @@
+// group of similar type of classes , interfaces and subpackages  .
+//built-in and user defined 
+// java package is used to categorize the classes and interfaces so they can be easily maintained 
+//access protection 
+//removes naming collision 
+//To Compile: javac -d . Name.java
+// To Run: java mypack.Name
