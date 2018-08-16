958c750e44013c55d677a9131a1338c65e0591e2
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-16 14:37:43 +0530

    Update Abstract.java

--- Abstract.java
+++ Abstract.java
@@ -21,3 +21,23 @@ public static void main(String args[]){
  obj.run();  
 }  
 }  
+
+
+//Example of an abstract class that has abstract and non-abstract methods  
+ abstract class Bike{  
+   Bike(){System.out.println("bike is created");}  
+   abstract void run();  
+   void changeGear(){System.out.println("gear changed");}  
+ }  
+//Creating a Child class which inherits Abstract class  
+ class Honda extends Bike{  
+ void run(){System.out.println("running safely..");}  
+ }  
+//Creating a Test class which calls abstract and non-abstract methods  
+ class TestAbstraction2{  
+ public static void main(String args[]){  
+  Bike obj = new Honda();  
+  obj.run();  
+  obj.changeGear();  
+ }  
+}  
