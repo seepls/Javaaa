8f8e777007184321375d0894807e08377cdfecf6
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-16 18:16:23 +0530

    Update Package.java

--- Package.java
+++ Package.java
@@ -5,3 +5,23 @@
 //removes naming collision 
 //To Compile: javac -d . Name.java
 // To Run: java mypack.Name
+here are three ways to access the package from outside the package.
+
+import package.*;
+import package.classname;
+fully qualified name.
+  //package then import then class.
+  //save by A.java  
+package pack;  
+public class A{  
+  public void msg(){System.out.println("Hello");}  
+}  
+
+//save by B.java  
+package mypack;  
+class B{  
+  public static void main(String args[]){  
+   pack.A obj = new pack.A();//using fully qualified name  
+   obj.msg();  
+  }  
+}  
