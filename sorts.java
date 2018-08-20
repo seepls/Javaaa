07f87538ff7eeec00b1318ad9ccd9dd0cd9c7b7b
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-20 19:15:44 +0530

    Update sorts.java

--- sorts.java
+++ sorts.java
@@ -101,3 +101,114 @@ public static void main(String[] args){
                 }
                 }
 
+
+//4. merge sort 
+// based on divide and conquere , divides into halfs : till the base then sorts at each level while merging 
+import java.io.*;
+import java.lang.*;
+class MergeSort{
+	void merge (int arr[] , int l , int m , int r)
+	{
+		int n1 = m-l+1 ;
+		int n2 = r-m ;
+		// temprory arrays 
+		int L[] = new int [n1] ;
+		int R[] = new int [n2 ]; 
+		//copy data in temprory arrays 
+		for(int i = 0 ; i < n1 ;i ++)
+			L[i] = arr[l+i];
+		for(int j =0 ;j<n2;j++)
+			R[j] = arr[m+1+j];
+	         int i = 0 , j = 0 ;
+		int k = l ;
+		while(i < n1 && j < n2 )
+		{
+			if(L[i]<=R[j]){
+				arr[k]= L[i];i++;}
+			else {arr[k] = R[j]; j++;}
+		k++;
+		}
+		 while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        /* Copy remaining elements of R[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    } 
+	void sort(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            // Find the middle point
+            int m = (l+r)/2;
+ 
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr , m+1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+ 
+        System.out.println("Given Array");
+        printArray(arr);
+ 
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
+ 
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
+
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
