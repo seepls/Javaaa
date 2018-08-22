c4226dbcb47f454476446ce615ff17fd0fa16cc9
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-22 18:07:29 +0530

    Update various.java

--- various.java
+++ various.java
@@ -309,3 +309,27 @@ int n = arr[0].length ;
 transpose(arr, n );
 }
 }
+
+//13. upper triangular matrix 
+import java.util.*;
+
+class Ani{
+public static void main(String args[]){
+int[][] a = { {1,2,3,5 } , { 3,4,5,0 },{3,5,6,7},{1,0,2,0}};
+int n= a.length;
+for(int i = 0 ; i< n ; i++)
+{ for(int j = 0 ;j < n ; j++){
+System.out.print(" " + a[i][j]);}
+System.out.println( "\n");
+
+}
+int sum = 0 ;
+System.out.println(" sum of upper triangle ");
+for(int j = 0 ; j< n ; j++){
+ for(int i = 0 ; i <= j ; i++)
+ {sum+= a[i][j] ;}
+}
+
+System.out.println("sum of upper triangular matrix :" + sum );
+
+}}
