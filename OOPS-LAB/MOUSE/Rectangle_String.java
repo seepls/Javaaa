2ae375d962418bf08f26cafcf2b748e5d9d830dd
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-28 21:47:52 +0530

    Create Rectangle_String.java

---
+++ OOPS-LAB/MOUSE/Rectangle_String.java
@@ -0,0 +1,4 @@
+//Rectangle when mouse released end points are place of mouse pressed and released , assume relased is south east of pressed 
+//do above with no such assumtion 
+//read a string and display in at the place of mouse click
+//read a string and dislay its ith letter at the location of ith click (nice)
