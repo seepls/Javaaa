6879f944ecf85d9b24dae4a2be85069acd2842a9
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-28 23:01:48 +0530

    Create Word.java

---
+++ OOPS-LAB/FUNCTION/Word.java
@@ -0,0 +1,13 @@
+// Rv1() : word wise reverse eg : " ram is bad" :: op : bas is ram 
+
+//Rv2() : returns all string with first letter capital 
+
+//Rv3() : reverse letter of word 
+
+//rev(String a , int s , int e) : reverse letter between s to e in string a 
+
+
+// String kth_string(String a , int k): returns kth letter of each word : 
+
+// String shuffle(String a ) : say a is string of words of equal length then shuffle collects all ith location and makes a word till k-1 location
+
