9d4fe108f46f163c0433f82d7a16371fb09dd159
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-28 16:13:47 +0530

    Create 5.java

---
+++ OOPS-LAB/GRAPHICS/5.java
@@ -0,0 +1,7 @@
+// part 1 :ram = red circle 2. gagan : blue 3. bimu : blue 
+//part2 : after every subsequence info radius increases by 5 
+// now replace while(1==1) by parameter conditonal while // and repaint() ; when a number 0  : window cleaned 
+//part 3 : modify above so that window is cleaned when "Hari" is typed 
+//(1) after cleaning window radius of new circle starts after the last circle drawn on applet 
+//(2) after cleaning  starts from radius reduced by 10 of last circle 
+//(3) : after first window clean starts from 50 , then 60 then 70 so on ...
