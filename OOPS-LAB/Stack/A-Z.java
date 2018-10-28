735144711dfbb61d513b19417114b6515696f6cd
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-28 14:30:33 +0530

    Create A-Z.java

---
+++ OOPS-LAB/Stack/A-Z.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Stack;
+class smriti
+{
+  public static void main(String args[])throws Exception
+  {Stack<String> s = new Stack <String>();
+  Stack <String> t = new Stack <String>();
+  DataInputStream u = new DataInputStream(System.in);
+  String a,b,c ;
+  char p,q ;
+  int i,j,size = 0 ;
+  do{a = u.readLine();
+  p = a.charAt(0);
+  if( p =='A')
+  {
+  }
+  if(p=='B')
+  {
+  }
+  if(p=='C'){
+  }
+  if(p=='D'){
+  }
+  
+  if(p=='E'){
+  }
+  if(p=='F'){
+  }
+  if(p=='G'){
+  }
+  if(p=='H'){
+  }
+  if(p=='I'){
+  }
+  if(p=='J'){
+  }
+  if(p=='K'){
+  }
+  if(p=='L'){
+  }
+  if(p=='M'){
+  }
+  if(p=='N'){
+  }
+  if(p=='P'){
+  }
+  if(p=='Q'){
+  }
+  if(p=='R'){
+  }
+  if(p=='W'){
+  }
+  if(p=='X'){
+  }
+  if(p=='Y'){
+  }
+  if(p=='Z'){
+  }
+  
+  }while(1==1);
+  }
+}
