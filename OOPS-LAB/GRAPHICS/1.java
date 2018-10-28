da6e6ce3169b7c40dd997890699027114070fa65
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-28 16:00:59 +0530

    Update 1.java

--- OOPS-LAB/GRAPHICS/1.java
+++ OOPS-LAB/GRAPHICS/1.java
@@ -1,1 +1,18 @@
-
+import java.awt.*;
+import java.io.*;
+import java.lang.*;
+import javax.swing.*;
+import java.applet.*;
+//reads a number x and creates a line from x , 100 to x+50,200
+public class ex extends Applet 
+{
+  int x = 70 ; String a =" " ;
+public void paint (Graphics g){
+do{
+  try{DataInputStream o = new DataInputStream(System.in);a=o.readLine();}
+  catch(Exception t ){}
+  x = Integer.parseInt(a);
+  g.drawLine(x,100,x+50,200);
+  
+}while(1==1);
+}
