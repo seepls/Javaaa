74ba77f226501b46bd69340966b463837a6556bc
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-28 16:15:28 +0530

    Create test.java

---
+++ OOPS-LAB/GRAPHICS/test.java
@@ -0,0 +1,3 @@
+/* take input x , y 
+draw square of side x , draw a circle inside the square then at the points where circle intersects the square draw a square 
+so on , the side x must always be greater than y 
