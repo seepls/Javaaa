5fefad96626c26d358ccf9cfb61d3e3afc57010d
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-28 22:54:05 +0530

    Create many_words_input.java

---
+++ OOPS-LAB/FUNCTION/many_words_input.java
@@ -0,0 +1,14 @@
+////String more ( String a ): returns all the words in String a. that have first letter repeated in themselves 
+
+//int cnt(String a , String b ) : returns the number of letters of string a present in b  repetion in count allowed 
+
+//int length(String a) : returns length of string 
+
+// String subset(String a, String b) : returns yes if  all letters of first word present in word 2 
+
+//String first(String a ) : returns first letter of all the words in the string 
+
+//String presentfirst(String a , String b ) :  returns first letter of the words present in string b 
+
+//String abcfirst(String a) : returns all those words whose each letter is start of someother word // use first and present (STAR QUESTION)
+
