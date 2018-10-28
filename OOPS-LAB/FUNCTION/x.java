ddeb2c117ed53012e0836be37d0fd7d2c525eaed
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-28 22:36:22 +0530

    Create x.java

---
+++ OOPS-LAB/FUNCTION/x.java
@@ -0,0 +1,23 @@
+// delkth : (String a , int k ) : returns string after deleting kth letter : numbering of a string 0 
+
+//delfirst(String a ) : delete first x in the string use above function 
+
+//delfirsttwo(String a ) : delete first two 'x' in the string // use delfirst twice 
+
+//secloc(String a ) : location of second 'x' in the string 
+
+//firstloc(String a): location of first 'x' : add 'x' at the bigining and then apply the secloc and so on 
+
+//delfirst(String a , int k ) delete first k 'x' : use firstloc() in loop 
+
+// delall(String a) : delete all 'x' in the string : use delfirst in while loop 
+
+// int count (String a ) : returns number of 'x' in the String 
+
+//int loc(String a , int k ) : location of kth x : find location of x after deleting k-1 x's and follow through 
+
+//String aft(String a , char b) : finds every first letter after b 
+
+//String mr(String a ): returns yes if first letter present more than once 
+
+
